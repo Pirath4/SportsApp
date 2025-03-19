77323cabc4af2fdd1d63171cdc22d5c0267a9f2a
Felipe <felipe290899@gmail.com>
2025-03-19 19:05:24 -0300

    att

--- lib/models/login_viewmodel.dart
+++ lib/models/login_viewmodel.dart
@@ -15,3 +15,19 @@ class LoginViewModel {
     }
   }
 }
+
+class CadastroViewModel {
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController senhaController = TextEditingController();
+
+  String fazerCadastro() {
+    String email = emailController.text;
+    String senha = senhaController.text;
+
+    if (email == "felipe" && senha == "123") {
+      return "Login realizado com sucesso!";
+    } else {
+      return "E-mail ou senha incorretos!";
+    }
+  }
+}
--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -9,68 +9,77 @@ class LoginView extends StatefulWidget {
 class _LoginViewState extends State<LoginView> {
   final LoginViewModel viewModel = LoginViewModel();
 
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    backgroundColor: Colors.blue[50], // Cor de fundo
-    body: Center(
-      child: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Bem vindo ao Friendly Sports"),
-            // Usando Row para colocar os ícones em linha
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center, // Centraliza os ícones
-              children: [
-                Icon(Icons.sports_soccer, size: 80, color: Colors.blue),
-                SizedBox(width: 20), // Espaço entre os ícones
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Friendly Sports'),
+        centerTitle: true,
+      ),
 
-                Icon(Icons.sports_volleyball, size: 80, color: Colors.blue),
-                SizedBox(width: 20), // Espaço entre os ícones
+      backgroundColor: Colors.blue[50], // Cor de fundo
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "Bem vindo ao Friendly Sports",
+              ),
 
-                Icon(Icons.sports_basketball, size: 80, color: Colors.blue),
-              ],
-            ),
-            SizedBox(height: 20),
-            TextField(
-              controller: viewModel.emailController,
-              decoration: InputDecoration(
-                labelText: "E-mail",
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.email),
+              // Usando Row para colocar os ícones em linha
+              Row(
+                mainAxisAlignment:
+                    MainAxisAlignment.center, // Centraliza os ícones
+                children: [
+                  Icon(Icons.sports_soccer, size: 80, color: Colors.blue),
+                  SizedBox(width: 20), // Espaço entre os ícones
+
+                  Icon(Icons.sports_volleyball, size: 80, color: Colors.blue),
+                  SizedBox(width: 20), // Espaço entre os ícones
+
+                  Icon(Icons.sports_basketball, size: 80, color: Colors.blue),
+                ],
               ),
-              keyboardType: TextInputType.emailAddress,
-            ),
-            SizedBox(height: 15),
-            TextField(
-              controller: viewModel.senhaController,
-              decoration: InputDecoration(
-                labelText: "Senha",
-                border: OutlineInputBorder(),
-                prefixIcon: Icon(Icons.lock),
+              SizedBox(height: 20),
+              TextField(
+                controller: viewModel.emailController,
+                decoration: InputDecoration(
+                  labelText: "E-mail",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.email),
+                ),
+                keyboardType: TextInputType.emailAddress,
               ),
-              obscureText: true,
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {
-                String message = viewModel.fazerLogin();
-                ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text(message)),
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+              SizedBox(height: 15),
+              TextField(
+                controller: viewModel.senhaController,
+                decoration: InputDecoration(
+                  labelText: "Senha",
+                  border: OutlineInputBorder(),
+                  prefixIcon: Icon(Icons.lock),
+                ),
+                obscureText: true,
               ),
-              child: Text("Entrar", style: TextStyle(fontSize: 18)),
-            ),
-          ],
+              SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () {
+                  String message = viewModel.fazerLogin();
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text(message)),
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                ),
+                child: Text("Login", style: TextStyle(fontSize: 18)),
+              ),
+            ],
+          ),
         ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
